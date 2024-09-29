6d7c78a77348739be74fbafe9bd746e68b001ca4
Dustin <dustin99wii@gmail.com>
2024-09-28 22:44:12 -0400

    Finished basic Lexer, beginning index

--- main.cpp
+++ main.cpp
@@ -8,12 +8,68 @@
 #include <cassert>
 #include <cstdint>
 #include <vector>
+#include <list>
+#include <map>
 
-uint16_t g_docId = 0;
+class Document;
+class Index;
+class Lexer;
+
+
 using Token = std::string;
+using Collection = std::vector<Document>;
+using DocId = uint16_t;
+
+DocId g_docId = 0;
+
+
+class Index {
+public:
+    Index() = delete;
+    Index(const Document& d) {
+
+    }
+
+    Index(std::vector<Document>& d) {
+
+    }
+
+    void AddDocument(const Document& d) {
+
+    }
+
+private:
+    struct TermInfo {
+        uint16_t doc_freq_;
+        std::list<uint16_t> postings_list_;
+    };
+
+    // Sorts automatically by key
+    std::map<Token, TermInfo> index_;
+};
+
 
 class Lexer {
+public:
+    Lexer() = delete;
+    explicit Lexer(const std::string& src) : src_{ src } {}
 
+    // for now, just split on whitespace
+    // create a real lexer later
+    void Lex(std::vector<Token>& tokens) {
+
+        std::string line;
+        while(std::getline(src_, line)) {
+            std::stringstream s { line };
+
+            std::string token;
+            while(std::getline(s, token, ' ')) {
+                tokens.push_back(token);
+            }
+        }
+    }
+private:
+    std::stringstream src_;
 };
 
 class Document {
@@ -36,6 +92,9 @@ public:
         assert(document.is_open() );
 
         buffer_ << document.rdbuf();
+
+        Lexer l { buffer_.str() };
+        l.Lex(tokens_);
     }
 private:
     uint16_t id_;
@@ -44,6 +103,11 @@ private:
 };
 
 int main() {
-    std::string extension  { "shakespeare.txt" };
-    Document d { extension };
+    std::string extension1  { "words1.txt" };
+    Document d1 { extension1 };
+
+    std::string extension2 { "words2.txt" };
+    Document d2 { extension2 };
+
+    return 0;
 }
